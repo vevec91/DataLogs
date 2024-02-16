981161c648dacb71991c466797cc36b70f1434a4
vevec91 <156291532+vevec91@users.noreply.github.com>
2024-02-16 12:16:49 +0100

    Updated by CrossUI No-Code App Builder

--- App/js/Data.js
+++ App/js/Data.js
@@ -15,25 +15,24 @@ xui.Class('App.Data', 'xui.Module',{
             var host=this, children=[], append=function(child){children.push(child.get(0));};
 
             // Ajoutez le code HTML et CSS pour le tableau
-            var tableHtml = `
-                <h2>Tableau Utilisateurs</h2>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Identifiant</th>
-                            <th>Mot de passe</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        ${this.properties.usersData.map(user => `
-                            <tr>
-                                <td>${user.id}</td>
-                                <td>${user.password}</td>
-                            </tr>
-                        `).join('')}
-                    </tbody>
-                </table>
-            `;
+           var tableHtml = '<h2>Tableau Utilisateurs</h2>' +
+                '<table>' +
+                    '<thead>' +
+                        '<tr>' +
+                            '<th>Identifiant</th>' +
+                            '<th>Mot de passe</th>' +
+                        '</tr>' +
+                    '</thead>' +
+                    '<tbody>' +
+                        this.properties.usersData.map(function(user) {
+                            return '<tr>' +
+                                       '<td>' + user.id + '</td>' +
+                                       '<td>' + user.password + '</td>' +
+                                   '</tr>';
+                        }).join('') +
+                    '</tbody>' +
+                '</table>';
+
 
             // Créez un élément DOM à partir du code HTML
             var tableElement = xui.create('<div>' + tableHtml + '</div>');
