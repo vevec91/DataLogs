be75deb59ddfa88e160f7061e4e7288dd7a25b96
vevec91 <156291532+vevec91@users.noreply.github.com>
2024-02-16 12:18:10 +0100

    Updated by CrossUI No-Code App Builder

--- App/js/Data.js
+++ App/js/Data.js
@@ -3,11 +3,8 @@ xui.Class('App.Data', 'xui.Module',{
         Dependencies: ['xui.css'],
         Required:[],
         properties : {
-            usersData: [
-                { id: 'utilisateur1', password: 'motdepasse1' },
-                { id: 'utilisateur2', password: 'motdepasse2' },
-                // Ajoutez d'autres données selon vos besoins
-            ]
+            // Initialisez la propriété usersData avec un tableau vide par défaut
+            usersData: []
         },
         initialize : function(){
         },
@@ -15,24 +12,24 @@ xui.Class('App.Data', 'xui.Module',{
             var host=this, children=[], append=function(child){children.push(child.get(0));};
 
             // Ajoutez le code HTML et CSS pour le tableau
-           var tableHtml = '<h2>Tableau Utilisateurs</h2>' +
-                '<table>' +
-                    '<thead>' +
-                        '<tr>' +
-                            '<th>Identifiant</th>' +
-                            '<th>Mot de passe</th>' +
-                        '</tr>' +
-                    '</thead>' +
-                    '<tbody>' +
-                        this.properties.usersData.map(function(user) {
-                            return '<tr>' +
-                                       '<td>' + user.id + '</td>' +
-                                       '<td>' + user.password + '</td>' +
-                                   '</tr>';
-                        }).join('') +
-                    '</tbody>' +
-                '</table>';
-
+            var tableHtml = '<h2>Tableau Utilisateurs</h2>' +
+                            '<table>' +
+                                '<thead>' +
+                                    '<tr>' +
+                                        '<th>Identifiant</th>' +
+                                        '<th>Mot de passe</th>' +
+                                    '</tr>' +
+                                '</thead>' +
+                                '<tbody>' +
+                                    // Vérifiez si usersData est défini avant de le mapper
+                                    (this.properties.usersData || []).map(function(user) {
+                                        return '<tr>' +
+                                                   '<td>' + user.id + '</td>' +
+                                                   '<td>' + user.password + '</td>' +
+                                               '</tr>';
+                                    }).join('') +
+                                '</tbody>' +
+                            '</table>';
 
             // Créez un élément DOM à partir du code HTML
             var tableElement = xui.create('<div>' + tableHtml + '</div>');
