be75883528148078ba0852644ef372acb66a7709
vevec91 <156291532+vevec91@users.noreply.github.com>
2024-02-16 12:30:00 +0100

    Updated by CrossUI No-Code App Builder

--- App/js/AdminLog.js
+++ App/js/AdminLog.js
@@ -1,7 +1,7 @@
 // The default code is a module class (inherited from xui.Module)
 // Ensure that all the value of "key/value pair" does not refer to external variables
 // Sub module don't support async functions
-xui.Class('App.Adminlog', 'xui.Module',{
+xui.Class('App.AdminLog', 'xui.Module',{
     Instance:{
         // Dependencies css
         Dependencies: [],
