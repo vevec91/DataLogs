066e680fbcd59db9202c30cfef08bf5b362ae004
vevec91 <156291532+vevec91@users.noreply.github.com>
2024-02-16 11:24:04 +0100

    Updated by CrossUI No-Code App Builder

--- App/js/AdminTool.js
+++ App/js/AdminTool.js
@@ -19,7 +19,47 @@ xui.Class('App.Name7', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.Input")
+                .setHost(host,"xui_ui_input8")
+                .setLeft("13.714285714285714em")
+                .setTop("12.19047619047619em")
+                .setWidth("18em")
+                .setLabelSize("8em")
+                .setLabelCaption("Input")
+            );
+            
+            append(
+                xui.create("xui.UI.Input")
+                .setHost(host,"xui_ui_input11")
+                .setLeft("13.714285714285714em")
+                .setTop("16em")
+                .setWidth("18em")
+                .setLabelSize("8em")
+                .setLabelCaption("Input")
+            );
+            
+            append(
+                xui.create("xui.UI.Input")
+                .setHost(host,"xui_ui_input12")
+                .setLeft("13.714285714285714em")
+                .setTop("19.047619047619047em")
+                .setWidth("18em")
+                .setLabelSize("8em")
+                .setLabelCaption("Input")
+            );
+            
+            append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button14")
+                .setLeft("43.42857142857143em")
+                .setTop("15.238095238095237em")
+                .setWidth("6.780952380952381em")
+                .setHeight("3.580952380952381em")
+                .setCaption("Button")
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
