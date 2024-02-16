0609da3eb16e1f4e7dedbcdf0ab59f9aa961e0b1
vevec91 <156291532+vevec91@users.noreply.github.com>
2024-02-16 12:14:39 +0100

    Updated by CrossUI No-Code App Builder

--- App/js/Data.js
+++ App/js/Data.js
@@ -1,39 +1,61 @@
-// The default code is a module class (inherited from xui.Module)
-// Ensure that all the value of "key/value pair" does not refer to external variables
-// Sub module don't support async functions
 xui.Class('App.Data', 'xui.Module',{
     Instance:{
-        // Dependencies css
-        Dependencies: [],
-        // Required modules
+        Dependencies: ['xui.css'],
         Required:[],
-        // To initialize properties
-        properties : {},
-
-        // To initialize instance(e.g. properties)
+        properties : {
+            usersData: [
+                { id: 'utilisateur1', password: 'motdepasse1' },
+                { id: 'utilisateur2', password: 'motdepasse2' },
+                // Ajoutez d'autres données selon vos besoins
+            ]
+        },
         initialize : function(){
         },
-
-        // To initialize internal components (mostly UI controls)
-        // *** If you're not a skilled, dont modify this function manually ***
         iniComponents : function(){
-            // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
 
+            // Ajoutez le code HTML et CSS pour le tableau
+            var tableHtml = `
+                <h2>Tableau Utilisateurs</h2>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Identifiant</th>
+                            <th>Mot de passe</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        ${this.properties.usersData.map(user => `
+                            <tr>
+                                <td>${user.id}</td>
+                                <td>${user.password}</td>
+                            </tr>
+                        `).join('')}
+                    </tbody>
+                </table>
+            `;
+
+            // Créez un élément DOM à partir du code HTML
+            var tableElement = xui.create('<div>' + tableHtml + '</div>');
+
+            // Ajoutez l'élément DOM au composant
+            append(tableElement);
+
             return children;
-            // ]]Code created by CrossUI RAD Studio
         },
-        // To determine how properties affects this module
         propSetAction : function(prop){
+            // Logique à exécuter lorsqu'une propriété est modifiée
         }
     },
-    // export
     Static:{
         $DataModel:{
+            // Définissez le modèle de données selon les besoins
         },
         $EventHandlers:{
+            // Ajoutez des gestionnaires d'événements si nécessaire
         },
         $Functions:{
+            // Ajoutez des fonctions statiques si nécessaire
         }
     }
 });
