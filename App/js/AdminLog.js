ad45a32e900b43928fcef9709f426f6c2a768fdb
vevec91 <156291532+vevec91@users.noreply.github.com>
2024-02-16 11:24:07 +0100

    Updated by CrossUI No-Code App Builder

--- App/js/AdminLog.js
+++ App/js/AdminLog.js
@@ -19,7 +19,57 @@ xui.Class('App.Name4', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.Input")
+                .setHost(host,"xui_ui_input6")
+                .setLeft("22.095238095238095em")
+                .setTop("17.523809523809526em")
+                .setWidth("18em")
+                .setLabelSize("8em")
+                .setLabelCaption("Password Input")
+                .setType("password")
+                .setValue("pwd")
+            );
+            
+            append(
+                xui.create("xui.UI.Input")
+                .setHost(host,"xui_ui_input7")
+                .setLeft("22.095238095238095em")
+                .setTop("11.428571428571429em")
+                .setWidth("18em")
+                .setLabelSize("8em")
+                .setLabelCaption("Input")
+            );
+            
+            append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button12")
+                .setLeft("29.714285714285715em")
+                .setTop("29.714285714285715em")
+                .setWidth("7.542857142857143em")
+                .setHeight("4.3428571428571425em")
+                .setCaption("Button")
+            );
+            
+            append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button20")
+                .setLeft("8.380952380952381em")
+                .setTop("6.095238095238095em")
+                .setCaption("Button")
+                .onClick([
+                    {
+                        "desc" : "Action 1",
+                        "type" : "page",
+                        "target" : "App",
+                        "args" : [true],
+                        "method" : "switch",
+                        "event" : 1
+                    }
+                ])
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
